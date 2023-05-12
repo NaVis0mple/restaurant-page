0ef111973ae39dccb73ec8c4f9c8ae14fada2cb3
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-12 17:30:36 +0800

    .

--- src/menu.js
+++ src/menu.js
@@ -4,7 +4,7 @@ const title = document.createElement('div')
 
 title.innerHTML = 'Best pizza maker in the world'
 title.classList.add('container')
-title.style.backgroundImage = "url('../../distImg/menu.JPG')"
+title.style.backgroundImage = "url('https://navis0mple.github.io/restaurant-page/distImg/menu.JPG')"
 title.style.backgroundSize = 'contain'
 title.style.height = '95vh'
 export { title }
