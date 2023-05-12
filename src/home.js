d921a8c549dab21bc06db7d2ea5808ebf585cfda
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-12 16:32:45 +0800

    .

--- src/home.js
+++ src/home.js
@@ -10,7 +10,7 @@ part2.innerHTML = 'pizzaaaaaaaaaaaa'
 title.append(part2)
 
 title.classList.add('container')
-title.style.backgroundImage = "url('./dist/distImg/photo.jpg')"
+title.style.backgroundImage = "url('dist/distImg/photo.jpg')"
 title.style.backgroundSize = 'contain'
 title.style.height = '95vh'
 
